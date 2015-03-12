90d9158944627c254ba51521cfa3c8ee46d4e05b
Rmccague <rjmvgm41@verizon.net>
2015-03-12 19:22:34 -0400

    Sample

---
+++ assignments.php
@@ -0,0 +1,100 @@
+
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+ <meta charset="utf-8">
+ <meta name="author" content="Robert M. Erickson">
+ <title>Syllabus - CS 142 (A) Advanced Web Design: CSS and Usability</title>
+ <meta name="description" content="This course provides an in depth exploration of the foundation of  Cascading Style Sheets and covering Web Usability to help you create a pleasing user experience for visitors to your site."/>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- https://rerickso.w3.uvm.edu/education/blackboard/css/base.css -->
+<!-- https://algoldma.w3.uvm.edu/cs142/assignment4/algoldma.css -->
+<link href="https://algoldma.w3.uvm.edu/cs142/assignment4/algoldma.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="https://algoldma.w3.uvm.edu/cs142/assignment4/print_algoldma.css" rel="stylesheet" type="text/css" media="print" />
+<link href="https://rmccague.w3.uvm.edu/cs142/assignment4/rmccague.css" rel="stylesheet" type="text/css" media="screen" />
+<link href="css/mainAdmin.css" rel="stylesheet" type="text/css" media="screen" />
+<link rel="shortcut icon" href="https://rerickso.w3.uvm.edu/education/blackboard/favicon.ico" >
+
+<!--[if lt IE 9]>
+    <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+<script type="text/javascript">
+<!--
+// keeps us from getting framed
+		if (top.location != self.location) {
+			top.location.replace(self.location)
+		}
+//-->
+</script>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-32182956-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+ })();
+</script>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<script>
+$(function() {
+     $( "#txtDateRequest" ).datepicker();
+});
+</script>
+
+<script src="misc.js"></script>
+</head>
+
+<body id="assignments">
+<div id="topbox"> <!-- ************ top box ************ -->
+<header> <!-- ************ header ************ -->
+		<h1 id="classtitle">
+<a href="http://www.uvm.edu/~cems/cs/?Page=default.php">CS</a> 142 (A) 
+<a href="syllabus.php?classRegNum=152" class="url">Advanced Web Design: CSS and Usability</a>
+</h1>
+<div id="toolmenu"><span id="screenSize"></span></div>
+</header> <!-- ************ end header  ************ -->
+
+<aside id="studentInfo"> <!-- %^%^%^%^%^ studentInfo %^%^%^%^%^ -->
+<span id="studentName">
+Student: algoldma [<a href="https://webauth.uvm.edu/webauth/logout">logout</a>]
+</span>
+<span id="studentGrade" >Current <a href="grades.php">Grade</a>: 78</span>
+<span id="classRank">
+Rank: 28 of 45
+</span>
+</aside> <!-- %^%^%^%^%^ student info %^%^%^%^%^ -->
+
+<nav id="mainMenu"> <!-- %^%^%^%^%^%^%^%^%^%^%^%^ Navigation %^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^%^% -->
+<ol>
+<li class="menuLink activeLink"><a href="assignments.php">Assignments</a></li>
+<li class="menuLink"><a href="calendar.php?abWeek=10">Calendar</a></li>
+<li class="menuLink"><a href="lectures.php">Lectures</a></li>
+<li class="menuLink"><a href="media.php">Resources</a></li>
+<li class="menuLink"><a href="sick.php">Sick</a></li>
+<li class="menuLink"><a href="syllabus.php">Syllabus</a></li>
+<li class="menuLink"><a href="submitAssignment.php">Submit Assignment</a></li>
+</ol>
+</nav> <!-- navigation -->
+</div> <!-- ************ top box ************ -->
+<!-- place admin menu code here -->
+<!-- userlevel = 1.5  -->
+<article id="mainContent"> <!-- ######################## main Content 
+######################## --><aside id="upcoming"><h1>Milestones</h1><div class="box"><h1>Assignment </h1><ol class="AssignmentLink"><li class="title summary"><a class="url" href="https://rerickso.w3.uvm.edu/education/blackboard/assign.php?pid=27">Redesign the Course Style</a> Friday the 13th</li></ol></div><div class="box"><h1>Test </h1><ol class="examLink"><li class="title summary">Exam II Wednesday the 25th</li></ol></div></aside><h2>Assignments</h2><ol class="AssignmentsLink"><li class="title summary">2015-01-23 - <a class="url" href="https://rerickso.w3.uvm.edu/education/blackboard/assign.php?pid=22">Style a Form</a> Date Submitted: Jan 23, 2015</li>
+<li class="title summary">2015-01-30 - Exam I</a></li><li class="title summary">2015-02-06 - <a class="url" href="https://rerickso.w3.uvm.edu/education/blackboard/assign.php?pid=24">Styling a Resume</a> Date Submitted: Feb 06, 2015</li>
+<li class="title summary">2015-02-20 - <a class="url" href="https://rerickso.w3.uvm.edu/education/blackboard/assign.php?pid=25">Style a Table</a> Date Submitted: Feb 20, 2015</li>
+<li class="title summary">2015-02-25 - Exam II</a></li><li class="title summary">2015-03-13 - <a class="url" href="https://rerickso.w3.uvm.edu/education/blackboard/assign.php?pid=27">Redesign the Course Style</a></li>
+<li class="title summary">2015-03-27 - Exam III</a></li><li class="title summary">2015-04-03 - Style Before and After</a></li><li class="title summary">2015-04-17 - Web Usability Testing</a></li><li class="title summary">2015-04-24 - Exam IV</a></li><li class="title summary">2015-04-30 - Attendance</a></li><li class="title summary">2015-05-04 - Final Project 12:00 am</a></li></ol></article> <!-- ########### main Content ########### -->
+<footer>
+<p>Web designed by: <span id="author"></span>
+</p>
+</footer> <!-- end footer -->
+</body>
+</html>
